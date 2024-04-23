b846ef41e41189c1c6c784a7710454ce83805169
Miraj Mainali <merazzmainali05@gmail.com>
2024-04-24 09:22:01 +1000

    design the ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <div className="box">
+        <div className="title">Expense Tracker</div>
+        <div className="show-balance">
+          <div className="balance-title">YOUR BALANCE</div>
+          <div className="balance-amount">$447.00</div>
+        </div>
+        <div className="income-expenses">
+          <div className="income">
+            <div className="income-title">INCOME</div>
+            <div className="income-amount">$1200</div>
+          </div>
+          <div className="expenses">
+            <div className="expenses-title">EXPENSES</div>
+            <div className="expenses-amount">$500</div>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
